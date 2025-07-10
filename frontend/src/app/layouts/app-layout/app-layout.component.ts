a38fe4951a3a1de27398d6b59f573e8d11130b60
Isabella Baumann <i.baumann@students.htl-leonding.ac.at>
2025-07-10 18:24:33 +0200

    navigation for content-management

--- frontend/src/app/layouts/app-layout/app-layout.component.ts
+++ frontend/src/app/layouts/app-layout/app-layout.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from "@angular/router";
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-app-layout',
   imports: [
     RouterLink,
-    RouterOutlet
+    RouterOutlet,
+    NgOptimizedImage
   ],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.css'
