dc9bfcc6c56536c71f9789bdd6b47ddbe8088104
Isabella Baumann <i.baumann@students.htl-leonding.ac.at>
2025-07-14 16:34:39 +0200

    windows problem solved start.bat

--- backend/start.bat
+++ backend/start.bat
@@ -1,18 +1,13 @@
-@echo off
-REM Skript bei Fehler abbrechen (Batch hat kein direktes Äquivalent, also manuell prüfen)
-
 echo Stopping existing containers...
 docker-compose down
 if errorlevel 1 (
   echo Fehler beim Stoppen der Container
-  exit /b 1
 )
 
 echo Building and starting containers...
-docker-compose up --build -d
+docker-compose up --build 
 if errorlevel 1 (
   echo Fehler beim Starten der Container
-  exit /b 1
 )
 
 echo Done!
