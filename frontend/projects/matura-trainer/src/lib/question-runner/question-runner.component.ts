9d44aad024ee06529b6789827a11d2474b2fa35c
GossenreiterThomas <t.gossenreiter@students.htl-leonding.ac.at>
2025-07-14 21:27:13 +0200

    added styling to the question, which also reacts to images. Bigger content if no image, smaller if image present. Also added real image links that get sent from the backend.

--- frontend/projects/matura-trainer/src/lib/question-runner/question-runner.component.ts
+++ frontend/projects/matura-trainer/src/lib/question-runner/question-runner.component.ts
@@ -1,22 +1,55 @@
 import {Component, inject, OnInit} from '@angular/core';
 import { Question } from '../../../../shared/src/lib/interfaces/question';
 import { QuestionService } from '../../../../shared/src/lib/services/question.service';
-import {NgForOf} from '@angular/common';
+import {NgForOf, NgStyle} from '@angular/common';
+import {FormArray, FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 
 @Component({
   selector: 'lib-question-runner',
   imports: [
-    NgForOf
+    NgForOf,
+    NgStyle,
+    ReactiveFormsModule
   ],
   templateUrl: './question-runner.component.html',
-  styleUrl: './question-runner.component.css'
+  styleUrls: [
+    './question-runner.component.css',
+    '../styles/shared-styles.css'
+  ]
 })
 export class QuestionRunnerComponent implements OnInit {
-  questionService: QuestionService = inject(QuestionService);
+  questionService = inject(QuestionService);
+  fb = inject(FormBuilder);
+
   question: Question | undefined;
+  form: FormGroup = this.fb.group({
+    answers: this.fb.array([])
+  });
 
   ngOnInit() {
-    this.questionService.getQuestionById(3).subscribe(q => {console.log(q); this.question = q;});
+    this.questionService.getQuestionById(7).subscribe(q => {
+      this.question = q;
+
+      const answerControls = this.fb.array(q.answers.map(() => false));
+      this.form.setControl('answers', answerControls);
+    });
+  }
+
+  get answersArray(): FormArray {
+    return this.form.get('answers') as FormArray;
+  }
+
+  anySelected(): boolean {
+    return this.answersArray?.controls.some(ctrl => ctrl.value);
+  }
+
+  submit(): void {
+    const selectedAnswers = this.answersArray.controls
+      .map((ctrl, index) => ctrl.value ? this.question?.answers[index] : null)
+      .filter(Boolean);
+
+    console.log('Selected answers:', selectedAnswers);
   }
 }
+
--- frontend/projects/shared/src/lib/interfaces/question.ts
+++ frontend/projects/shared/src/lib/interfaces/question.ts
@@ -1,5 +1,6 @@
 import {Answer} from './answer';
 import {TopicPool} from './topic-pool';
+import {Media} from './media';
 
 export interface Question {
   id: number;
@@ -10,4 +11,5 @@ export interface Question {
   isPublic: boolean;
   answers: Answer[];
   topicPool: TopicPool;
+  media: Media;
 }
