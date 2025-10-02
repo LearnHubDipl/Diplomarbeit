c5579296f9fb76db76d861ddf7f8dcf68ec08df1
GossenreiterThomas <t.gossenreiter@students.htl-leonding.ac.at>
2025-10-02 15:06:21 +0200

    solved issue with the mock data importer where null fields were given a default value, this issue also fixes how the questions in the entries were unanswered

--- frontend/projects/shared/src/lib/services/globals.ts
+++ frontend/projects/shared/src/lib/services/globals.ts
@@ -6,4 +6,4 @@
 // export const API_BASE_URL = "https://vm91.htl-leonding.ac.at/backend/api"
 
 // dev API URL
-export const API_BASE_URL = "https://vm91.htl-leonding.ac.at/backend/api"
+export const API_BASE_URL = "http://localhost:8080/api"
